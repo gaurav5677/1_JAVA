093340f51a3079b0f96fd1af46226e61d482108e
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-14 10:27:18 +0530

    day1 average Marks

---
+++ src/Basic/AverageMarks.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class AverageMarks {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char ch = s.next().charAt(0);
+        int  m1,m2,m3;
+        m1 = s.nextInt();
+        m2 = s.nextInt();
+        m3 = s.nextInt();
+        int avg=(m1+m2+m3)/3;
+        System.out.println(ch+" "+avg);
+
+    }
+}
