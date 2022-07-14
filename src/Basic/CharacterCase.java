42e5cb0be69af156102f821e3b1360e2191fc7ca
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-14 11:13:55 +0530

    day 1 CharacterCase

---
+++ src/Basic/CharacterCase.java
@@ -0,0 +1,20 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class CharacterCase {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char ch = s.next().charAt(0);
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println(1);
+        }
+         else if (ch >= 'a' && ch <= 'z') {
+            System.out.println(0);
+        }
+        else  {
+            System.out.println(-1);
+
+        }
+    }
+}
