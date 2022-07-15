21d17072f9edb1aa3425c991eb8ca9fa849a909c
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-15 19:48:00 +0530

    day 2 array

---
+++ src/Array/ArrayIntro.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayIntro {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter element at " + i + " th index");
+            arr[i]=s.nextInt();
+        }
+
+        for (int i = 0; i <n ; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+}
+
+
---
+++ src/Array/array1.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class array1 {
+    public static void main(String[] args) {
+        int[] arr = new int [10];
+        arr[0]=5;
+        arr[5]=18;
+        System.out.println(arr[0]);
+        System.out.println(arr[5]);
+
+
+        char [] c = new char [10];
+        double [] d = new double[10];
+        System.out.println(c[0]);//will print the null array
+        //for charter array it is always a null character by default
+
+        System.out.println(d[0]);
+
+
+    }
+}
