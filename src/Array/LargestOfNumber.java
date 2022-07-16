509c3988460116b12831b1faec82b9091767378a
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-16 21:57:53 +0530

    day 3 LargestINArray

---
+++ src/Array/LargestOfNumber.java
@@ -0,0 +1,34 @@
+package Array;
+
+import java.util.Scanner;
+
+public class LargestOfNumber {
+    public static int[] TakeInput(){
+        Scanner s = new Scanner(System.in);
+         int n = s.nextInt();
+         int []arr = new int[n];
+         for( int i = 0 ; i<n;i++){
+             System.out.println("Enter the element at"+i+"th position");
+             arr[i]=s.nextInt();
+         }
+         return arr;
+    }
+
+
+
+    public static int largest(int[]arr){
+        int max = Integer.MIN_VALUE;
+        for( int i = 0 ; i< arr.length;i++){
+            if( arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return  max;
+
+    }
+    public static void main(String[] args) {
+        int [] arr = TakeInput();
+        int  lar = largest(arr);
+        System.out.println(lar);
+    }
+}
