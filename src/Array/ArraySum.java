58dbef06e43f08b0ef2c11f528d862276e3e3f30
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-16 10:28:18 +0530

    day 3 ArraySum

---
+++ src/Array/ArraySum.java
@@ -0,0 +1,39 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArraySum {
+    public static int[] takeInput() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the element " + i + "ith position: ");
+            arr[i] = s.nextInt();
+
+        }
+        return arr;
+
+    }
+
+    public static void arraysum(int[]arr) {
+        int total = 0 ;
+        for ( int i = 0 ;i< arr.length;i++){
+             total = total+arr[i];
+        }
+        System.out.println(total);
+    }
+
+//    public static void output(int[] arr) {
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//        }
+//
+//    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        arraysum(arr);
+
+    }
+}
