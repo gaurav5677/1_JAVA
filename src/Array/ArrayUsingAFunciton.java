5b0d4e037d83e538c4c4c2fa3f8f78c56d8593cb
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-16 10:09:50 +0530

    day 3 arrayUsingFunction

---
+++ src/Array/ArrayUsingAFunciton.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ArrayUsingAFunciton {
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+
+    public static int[] takeInput() {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter Element at " + i + "th index");
+            arr[i] = s.nextInt();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = takeInput();
+        printArray(arr);
+
+    }
+
+}
+
