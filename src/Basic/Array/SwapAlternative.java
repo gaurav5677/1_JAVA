d5c72348cb33e61d26c31d4531de153bd0e4c2a0
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-18 11:36:54 +0530

    day 5 SwapAlternative elements of the array

---
+++ src/Basic/Array/SwapAlternative.java
@@ -0,0 +1,23 @@
+package Basic.Array;
+
+import java.util.Arrays;
+
+public class SwapAlternative {
+    public static void Swap(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {9, 3, 6, 12, 4, 32,5,234};
+      Swap(arr);
+    }
+}
+
