e511c3210b2a26438e457676f3e8cd4e4c059e01
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-23 22:22:49 +0530

    day 10

--- src/Basic/_4Array2SearchingAlgo/_6PushZerosToEnd.java
+++ src/Basic/_4Array2SearchingAlgo/_6PushZerosToEnd.java
@@ -1,4 +1,33 @@
 package Basic._4Array2SearchingAlgo;
 
 public class _6PushZerosToEnd {
+    public static void main(String[] args) {
+        int[] arr = {3, 0, 2, 3, 4, 0, 0, 0, 4};
+        pushZerostoEnd(arr);
+        printArray(arr);
+    }
+
+    public static void pushZerostoEnd(int[] arr) {
+        int k = 0; //sorted array Index , it will update only when nonzero element found ..
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {// here we are checking whether the next num is zero or not
+                //if it is non zero then we well replace it with the temp variable and
+                // we will increment i but not the k ,  k will  increment when  non zero element is found
+                int temp = arr[i];
+                arr[i] = arr[k]; //arr{i}= arr{nonzero}
+                arr[k] = temp;//arr{nonzero}=temp;
+                k++;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
 }
+
+
