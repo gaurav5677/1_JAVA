9ce3db7a3ce91f44998feae94af5091588c7d1a7
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-27 23:34:23 +0530

    day 12

---
+++ src/Basic/Strings/_5CoundWords.java
@@ -0,0 +1,22 @@
+package Basic.Strings;
+
+
+import java.util.Scanner;
+
+public class _5CoundWords {
+    public static void main(String[] args) {
+        Scanner s = new Scanner( System.in);
+        String  str = s.nextLine();
+        printChars(str);
+    }
+
+    public static void printChars( String str ){
+         int i ;
+        for (i=0; i <str.length() ; i++) {
+
+
+        }
+        System.out.println(i);
+
+    }
+}
