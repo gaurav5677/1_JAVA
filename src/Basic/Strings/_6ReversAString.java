decb3439008247fadd66fbbe19c3c747f8f16efd
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-27 23:35:06 +0530

    day 14

---
+++ src/Basic/Strings/_6ReversAString.java
@@ -0,0 +1,24 @@
+package Basic.Strings;
+
+public class _6ReversAString {
+    public static String  ReversStr( String str ){
+ String  reversStr=" ";
+//        for (int i = str.length()-1; i >=0 ; i--) {
+//            reversStr=reversStr+str.charAt(i);
+//
+//        }
+
+
+        //method 2
+        for (int i = 0; i <=str.length()-1 ; i++) {
+            reversStr= str.charAt(i)+reversStr;
+
+        }
+        return reversStr;
+    }
+    public static void main(String[] args) {
+ String str = "abcde";
+     String ReversString = ReversStr(str);
+        System.out.println(ReversString);
+    }
+}
