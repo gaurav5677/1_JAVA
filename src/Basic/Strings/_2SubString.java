0eb61ddafd0cbe088da49c9f70cacef95630dde2
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-27 23:33:30 +0530

    day 12

---
+++ src/Basic/Strings/_2SubString.java
@@ -0,0 +1,10 @@
+package Basic.Strings;
+
+public class _2SubString {
+    public static void main(String[] args) {
+        String str1 = "India";
+        String substr = str1.substring(1,5 );
+        System.out.println(substr);
+        System.out.println(substr.length())     ;
+    }
+}
