a8f9dceba6265915987c84d16fb90204ca3672ad
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-28 23:49:06 +0530

    day 15

--- src/Basic/_4Array2SearchingAlgo/asdjhfkjdf.java
+++ src/Basic/_4Array2SearchingAlgo/asdjhfkjdf.java
@@ -2,12 +2,10 @@ package Basic._4Array2SearchingAlgo;
 
 public class asdjhfkjdf {
     public static void main(String[] args) {
-         String n = "Gaurav patil";
-         int i;
-         for( i = 0 ; i<n.length();i++){
-             if(n.charAt(i)==' ' ){
-                 System.out.println(i);
-             }
-         }
+        String str1 = "abc";
+        String str2 = "";
+        System.out.println(str1.contains(str2));
     }
 }
+
+
