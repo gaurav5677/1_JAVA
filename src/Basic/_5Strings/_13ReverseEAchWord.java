07ec252020a7b98d3dbf1f9c6b6922c246b88f1b
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-30 23:51:37 +0530

    day 16+1

--- src/Basic/_5Strings/_13ReverseEAchWord.java
+++ src/Basic/_5Strings/_13ReverseEAchWord.java
@@ -1,4 +1,38 @@
 package Basic._5Strings;
 
 public class _13ReverseEAchWord {
+    public static void main(String[] args) {
+
+
+        String str ="always drink 3L water ";
+
+        System.out.println(ReversEachW(str));
+    }
+
+    public static String reverseword(String str ,int start,int end) {
+        String reverse = " ";
+
+        while (start < end) {
+            reverse = str.charAt(start) + reverse;
+            ++start;
+        }
+        return reverse;
+    }
+
+
+    public static String ReversEachW(String s) {
+        int n = s.length();
+        int previousSpaceIndex = -1;
+        String Ans = " ";
+        int i = 0;
+        for (; i < n; i++) {
+            if (s.charAt(i) == ' ') {
+                Ans += (reverseword(s, previousSpaceIndex + 1, i) + " ");
+                previousSpaceIndex = i;
+            }
+        }
+        Ans += (reverseword(s, previousSpaceIndex + 1, i) + " ");
+        return Ans;
+
+    }
 }
