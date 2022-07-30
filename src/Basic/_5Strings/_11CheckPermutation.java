89c5b60aaa818fbec88450a612f09c8a72d72dbc
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-30 23:50:43 +0530

    day 16+1

--- src/Basic/_5Strings/_11CheckPermutation.java
+++ src/Basic/_5Strings/_11CheckPermutation.java
@@ -1,4 +1,38 @@
 package Basic._5Strings;
 
 public class _11CheckPermutation {
+    public static void main(String[] args) {
+        String s1 = "stingr";
+        String s2 = "string";
+        System.out.println(CheckPer(s1, s2));
+    }
+
+    public static boolean CheckPer(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int fre[] = new int[256];
+        for (int i = 0; i < 256; i++) {
+            fre[i] = 0;
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            char ch = str1.charAt(i);
+            ++fre[ch];
+
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            char ch = str2.charAt(i);
+            --fre[ch];
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (fre[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
 }
