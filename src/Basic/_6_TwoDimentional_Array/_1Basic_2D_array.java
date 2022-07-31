06f0a149df6f5e4b2828289b9139e7e6d0ab49e3
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-07-31 23:53:02 +0530

    day 18

---
+++ src/Basic/_6_TwoDimentional_Array/_1Basic_2D_array.java
@@ -0,0 +1,16 @@
+package Basic._6_TwoDimentional_Array;
+
+public class _1Basic_2D_array {
+    public static void main(String[] args) {
+
+
+        int[][] arr2d = new int[3][4];
+        System.out.println(arr2d[1][2]);
+
+        arr2d[2][1]=12;
+        System.out.println(arr2d[2][1]);
+//        System.out.println(arr2d[3][1]);
+    }
+
+
+}
