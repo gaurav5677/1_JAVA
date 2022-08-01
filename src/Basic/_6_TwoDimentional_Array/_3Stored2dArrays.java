8470d06e49b9f259ea91578a3b73d5bfc623ab7c
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-01 22:17:42 +0530

    day 19

---
+++ src/Basic/_6_TwoDimentional_Array/_3Stored2dArrays.java
@@ -0,0 +1,10 @@
+package Basic._6_TwoDimentional_Array;
+
+public class _3Stored2dArrays {
+    public static void main(String[] args) {
+        int [] [ ] arr2d={{1,2,3,4},{2,3,4,5},{3,4,5,6}};
+        System.out.println(arr2d.length);
+        System.out.println(arr2d[0].length);
+    }
+
+}
