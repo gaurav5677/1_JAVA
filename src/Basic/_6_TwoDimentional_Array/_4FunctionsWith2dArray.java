d194106aa5fad13c54377735c9cf648c9ee2fdc1
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-02 23:33:20 +0530

    day 20

--- src/Basic/_6_TwoDimentional_Array/_4FunctionsWith2dArray.java
+++ src/Basic/_6_TwoDimentional_Array/_4FunctionsWith2dArray.java
@@ -1,37 +1,43 @@
 package Basic._6_TwoDimentional_Array;
 
+
 import java.util.Scanner;
 
 public class _4FunctionsWith2dArray {
+
+
     public static void main(String[] args) {
-        int [][] input=takeInput();
+        int[][] input = takeInput();
         printArray(input);
 
     }
 
-    public static void printArray(int[][] arr){
+    public static void printArray(int[][] arr) {
         int rows = arr.length;
-        int cols=arr[0].length;
-        for (int i = 0; i <rows ; i++) {
-            for (int j = 0; j <cols ; j++) {
+        int cols = arr[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
 
-                System.out.print(arr[i][j]+" ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public static int[][] takeInput(){
+    public static int[][] takeInput() {
         Scanner s = new Scanner(System.in);
-        int rows=s.nextInt();
-        int cols=s.nextInt();
-        int [][] arr=new int[rows][cols];
+        System.out.println("Enter the number of rows  ");
+        int rows = s.nextInt();
+        System.out.println("Enter the number of cols ");
+        int cols = s.nextInt();
+        int[][] arr = new int[rows][cols];
 
-        for (int i = 0 ; i<rows ;i++){
-            for( int j = 0 ; j< cols;j++){
-                arr[i][j]=s.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arr[i][j] = s.nextInt();
             }
         }
         return arr;
+
     }
 }
