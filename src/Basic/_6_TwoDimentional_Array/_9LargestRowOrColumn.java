59cd64796de05e833bedff7b86958f8ddb458047
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-02 23:34:51 +0530

    day 20

---
+++ src/Basic/_6_TwoDimentional_Array/_9LargestRowOrColumn.java
@@ -0,0 +1,7 @@
+package Basic._6_TwoDimentional_Array;
+
+public class _9LargestRowOrColumn {
+//    For a given two-dimensional integer array/list of size (N x M), you need to find out which row or column has the
+//    largest sum(sum of all the elements in a row/column) amongst all the rows and columns.
+
+}
