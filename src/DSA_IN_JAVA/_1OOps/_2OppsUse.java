5e5774bbc48e68b012393b948abe20264f612b23
$Gaurav <85819394+gaurav5677@users.noreply.github.com>
2022-08-06 23:35:05 +0530

    day 24

--- src/DSA_IN_JAVA/_1OOps/_2OppsUse.java
+++ src/DSA_IN_JAVA/_1OOps/_2OppsUse.java
@@ -9,7 +9,7 @@ public class _2OppsUse {
         Scanner s = new Scanner(System.in);
         _1OOPS s1 = new _1OOPS();// here we create object by using "new." // here we get new _100Ps object which have all the properties.
         _1OOPS s2 = new _1OOPS();
-        // here s2 and s1 are the refreance
+        // here s2 and s1 are the refreance Containing addresses
         System.out.println(s1);
         s1.name="hey there.";
         s1.rollNumber=10;
